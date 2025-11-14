fba33ddb547c299c482c946c9bc5c0d46cd7be0e
Teo Kets <ketsetzidis909@gmail.com>
2025-11-14 23:28:24 +0200

    tried it with real images

--- api.py
+++ api.py
@@ -4,12 +4,11 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 
-
 app = FastAPI()
 
 model = tf.keras.models.load_model("models/emotion_model.keras")
 
-IMG_SIZE = (127, 128)
+IMG_SIZE = (128, 128)  # match training
 
 
 @app.post("/predict")
@@ -17,13 +16,14 @@ async def predict_emotion(file: UploadFile = File(...)):
     image = Image.open(file.file).convert("L")  # grayscale
     image = image.resize(IMG_SIZE)
 
-    img_array = np.array(image) / 254.0
-    img_array = np.expand_dims(img_array, axis=-1)
+    img_array = np.array(image, dtype=np.float32)
+
+    img_array = np.expand_dims(img_array, axis=-1)  # (128,128,1)
+    img_array = np.expand_dims(img_array, axis=0)  # (1,128,128,1)
 
     prediction = model.predict(img_array)
-    emotion_index = int(np.argmax(prediction))
+    emotion_index = int(np.argmax(prediction, axis=1)[0])
     confidence = float(np.max(prediction))
 
-    LABELS = ["Surprise", "Sad", "Happy", "Fear", "Angry"]
-
+    LABELS = ["Angry", "Fear", "Happy", "Sad", "Suprise"]
     return JSONResponse({"emotion": LABELS[emotion_index], "confidence": confidence})
